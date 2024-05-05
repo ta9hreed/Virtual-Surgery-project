f1224b59e98e02cd794982a4b404306983b2a065
ta9hreed <taghreedmohamed204@gmail.com>
2024-05-06 01:29:33 +0300

    vercal e commit

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -6,12 +6,34 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure:true,
+    //secure:true,
     
 });
 
+const fs = require('fs');
+// Cloudinary upload image
+const cloudinaryUploadImage = async (fileToUpload) => {
+    try {
+        // Check if the file exists
+        if (!fs.existsSync(fileToUpload)) {
+            throw new Error('File does not exist');
+        }
+
+        // Upload the file to Cloudinary
+        const data = await cloudinary.uploader.upload(fileToUpload, {
+            resource_type: 'auto',
+        });
+
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+
+
+
 //cloudinary upload image
-const cloudinaryUploadImage= async (fileToUpload)=>{
+/*const cloudinaryUploadImage= async (fileToUpload)=>{
     try {
         const data = await cloudinary.uploader.upload(fileToUpload,{
             resource_type:'auto',
@@ -21,7 +43,8 @@ const cloudinaryUploadImage= async (fileToUpload)=>{
         return error;
         
     }
-}
+}*/
+
 //cloudinary remove image
 const cloudinaryRemoveImage= async (imagePublicId)=>{
     try {
