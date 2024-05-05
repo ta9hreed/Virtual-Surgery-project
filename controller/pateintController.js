a1a6b45c7cd9ea4563f4894006f745ba025b4f48
ta9hreed <taghreedmohamed204@gmail.com>
2024-05-05 15:01:18 +0300

    mri scan update commit

--- controller/pateintController.js
+++ controller/pateintController.js
@@ -1,25 +1,59 @@
 const asyncHandler = require("express-async-handler");
-const {Patient,validateCreatePatient,validateUpdatePatient} =require("../models/PatientModel");
-const{ MRIScan }=require("../models/MRimodel");
+const { Patient, validateCreatePatient, validateUpdatePatient } = require("../models/PatientModel");
+const { MRIScan } = require("../models/MRimodel");
 
 /**
 @desc Get all patients
 @route /api/patients
 @method GET
 @access Public
+**/
+module.exports.paginationPatients = asyncHandler(async (req, res) => {
+    const patientPerPage = 3;
+    const { pageNumber } = req.query;
+    let PatientList;
 
-*/ 
+    if (pageNumber) {
+        PatientList = await Patient.find({ user: req.user.id })
+            .skip((pageNumber - 1) * patientPerPage)
+            .limit(patientPerPage)
+            .populate("user", ["FristName", "LastName"], null, { virtuals: false })
+            .sort({ createdAt: -1 });
+
+    }
+    else {
+        PatientList = await Patient.find({ user: req.user.id })
+            .populate("user", ["FristName", "LastName"]).sort({ createdAt: -1 });
+
+    }
+    res.status(200).json(PatientList);
+
+});
+
 //skip jump in first about number of write
 //limit  limit the result by this number 2 print 2 items
 
-module.exports.getAllPatient=asyncHandler(async(req,res)=>{
-    const {pageNumber}=req.query;
-    const patientPerPage=2;
-    const PatientList=await Patient.find().skip((pageNumber-1)*patientPerPage).limit(patientPerPage)
-    .populate("user",["_id","FristName","LastName"]);
-    res.status(200).json(PatientList);
-});
+/*module.exports.getAllPatient = asyncHandler(async (req, res) => {
+    const patientPerPage = 3;
+    const { pageNumber } = req.query;
+    let PatientList;
 
+    if (pageNumber) {
+        PatientList = await Patient.find({ user: req.user.id })
+            .skip((pageNumber - 1) * patientPerPage)
+            .limit(patientPerPage)
+            .populate("user", ["_id", "FristName", "LastName"]).sort({ createdAt: -1 });;
+
+    }
+    else {
+        PatientList = await Patient.find({ user: req.user.id })
+            .populate("user", ["_id", "FristName", "LastName"]).sort({ createdAt: -1 });
+
+    }
+    res.status(200).json(PatientList);
+
+});
+*/
 
 /**
 @desc Get  all patient 
@@ -27,11 +61,13 @@ module.exports.getAllPatient=asyncHandler(async(req,res)=>{
 @method GET
 @access Public
 
-*/ 
-module.exports.getPatients = asyncHandler(async(req,res)=>{
+*/
+module.exports.getPatients = asyncHandler(async (req, res) => {
 
-    const PatientList=await Patient.find()
-    .populate("user",["_id","FristName","LastName"]).sort({createdAt:-1});
+
+
+    const PatientList = await Patient.find({ user: req.user.id })
+    //.populate("user", ["_id", "FristName", "LastName"]).sort({ createdAt: -1 });
     res.status(200).json(PatientList);
 }
 
@@ -45,17 +81,24 @@ module.exports.getPatients = asyncHandler(async(req,res)=>{
 
 */
 
-module.exports.getPatientByID=asyncHandler(async(req,res)=>{
-    
-    const patient= await Patient.findById(req.params.id).
-    populate("user",["-Password"]);;
-if(patient){
-    res.status(200).json(patient);
-}
-else{
-    res.status(404).json({message:'The patient with the given ID was not found.'})
-}
-    
+module.exports.getPatientByID = asyncHandler(async (req, res) => {
+    const patient = await Patient.findById(req.params.id);
+
+    if (!patient) {
+
+        res.status(404).json({ message: 'The patient with the given ID was not found.' })
+    }
+    if (req.user.id !== patient.user.toString()) {
+        return res.status(403).json({ message: "access denied,forddien" });
+    }
+
+    else {
+        await Patient.findById(req.params.id).
+            populate("user", ["-Password"]);
+
+        res.status(200).json(patient);
+    }
+
 
 }
 );
@@ -67,91 +110,91 @@ else{
 @access private in log in
 
 */
-module.exports.addPatient=asyncHandler( async (req,res)=>{
-    
-    const {error}=validateCreatePatient(req.body);
-        if (error) {
+module.exports.addPatient = asyncHandler(async (req, res) => {
+
+    const { error } = validateCreatePatient(req.body);
+    if (error) {
         res.status(400).send(error.details[0].message);
-        }
-        
-            const patient= await  Patient.create({
-                user:req.user.id,
-                FristName:req.body.FristName,
-                LastName:req.body.LastName,
-                Age:req.body.Age,
-                Gender:req.body.Gender,
-                RiskFactorsAndLifeStyle:req.body.RiskFactorsAndLifeStyle,
-                FamilyHistory:req.body.FamilyHistory,
-                NeurologicalExam:req.body.NeurologicalExam,
-                Symptoms:req.body.Symptoms,
-                TreatmentHistory:req.body.TreatmentHistory,
-                Allergies:req.body.Allergies,
-                DurationAndProgressionOfSymptoms:req.body.DurationAndProgressionOfSymptoms,
-                Diagnosis:req.body.Diagnosis,
-                MedicalHistory:req.body.MedicalHistory,
-                Notes:req.body.Notes,
-                BiopsyOrPathologyResults:req.body.BiopsyOrPathologyResults,
-                LabTestResult:req.body.LabTestResult,
-                CurrentMedications:req.body.CurrentMedications,
-                
-            });
-            
-            res.status(201).json(patient);
-            
-    
-        
+    }
+
+    const patient = await Patient.create({
+        user: req.user.id,
+        FristName: req.body.FristName,
+        LastName: req.body.LastName,
+        Age: req.body.Age,
+        Gender: req.body.Gender,
+        RiskFactorsAndLifeStyle: req.body.RiskFactorsAndLifeStyle,
+        FamilyHistory: req.body.FamilyHistory,
+        NeurologicalExam: req.body.NeurologicalExam,
+        Symptoms: req.body.Symptoms,
+        TreatmentHistory: req.body.TreatmentHistory,
+        Allergies: req.body.Allergies,
+        DurationAndProgressionOfSymptoms: req.body.DurationAndProgressionOfSymptoms,
+        Diagnosis: req.body.Diagnosis,
+        MedicalHistory: req.body.MedicalHistory,
+        Notes: req.body.Notes,
+        BiopsyOrPathologyResults: req.body.BiopsyOrPathologyResults,
+        LabTestResult: req.body.LabTestResult,
+        CurrentMedications: req.body.CurrentMedications,
+
     });
-    /**
+
+    res.status(201).json(patient);
+
+
+
+});
+/**
 @desc update all patients
 @route /api/patients/:id
 @method put
 @access Public
 
 */
-module.exports.updatePatient=asyncHandler(async(req,res)=> {
-    const {error}= validateUpdatePatient(req.body);
+module.exports.updatePatient = asyncHandler(async (req, res) => {
+    const { error } = validateUpdatePatient(req.body);
     if (error) {
-        res.status(400).json({message:error.details[0].message});
+        res.status(400).json({ message: error.details[0].message });
     }
-    const patient=await Patient.findById(req.params.id)  ;
-    if(!patient){
-        res.status(404).json({message:"patient not found"})
+    const patient = await Patient.findById(req.params.id);
+    if (!patient) {
+        res.status(404).json({ message: "patient not found" })
     }
-    if(req.user.id!== patient.user.toString()){
-        return res.status(403).json({message:"access denied,forddien"});
+    if (req.user.id !== patient.user.toString()) {
+        return res.status(403).json({ message: "access denied,forddien" });
     }
 
-    const updatepatient =  await Patient.findByIdAndUpdate(
+    const updatepatient = await Patient.findByIdAndUpdate(
         req.params.id,
         {
             $set: {
-                FristName:req.body.FirstName,
-                LastName:req.body.LastName,
-                Age: req.body.Age ,
-                Gender: req.body.Gender ,
-                RiskFactorsAndLifeStyle:req.body.RiskFactorsAndLifeStyle,
-                FamilyHistory:req.body.FamilyHistory,
-                NeurologicalExam:req.body.NeurologicalExam,
-                Symptoms:req.body.Symptoms,
-                TreatmentHistory:req.body.TreatmentHistory,
-                Allergies:req.body.Allergies,
-                DurationAndProgressionOfSymptoms:req.body.DurationAndProgressionOfSymptoms,
-                Diagnosis:req.body.Diagnosis,
-                MedicalHistory:req.body.MedicalHistory,
-                Notes:req.body.Notes,
-                BiopsyOrPathologyResults:req.body.BiopsyOrPathologyResults,
-                LabTestResult:req.body.LabTestResult,
-                CurrentMedications:req.body.CurrentMedications,
-        
-            
-    
+                FristName: req.body.FirstName,
+                LastName: req.body.LastName,
+                Age: req.body.Age,
+                Gender: req.body.Gender,
+                RiskFactorsAndLifeStyle: req.body.RiskFactorsAndLifeStyle,
+                FamilyHistory: req.body.FamilyHistory,
+                NeurologicalExam: req.body.NeurologicalExam,
+                Symptoms: req.body.Symptoms,
+                TreatmentHistory: req.body.TreatmentHistory,
+                Allergies: req.body.Allergies,
+                DurationAndProgressionOfSymptoms: req.body.DurationAndProgressionOfSymptoms,
+                Diagnosis: req.body.Diagnosis,
+                MedicalHistory: req.body.MedicalHistory,
+                Notes: req.body.Notes,
+                BiopsyOrPathologyResults: req.body.BiopsyOrPathologyResults,
+                LabTestResult: req.body.LabTestResult,
+                CurrentMedications: req.body.CurrentMedications,
+
+
+
             }
-        },{new:true}).populate("user",["-Passward"]);
-        res.status(200).json(updatepatient);
-        
-        
-    
-        
+        }, { new: true }).populate("user", ["-Passward"]);
+    res.status(200).json(updatepatient);
+
+
+
+
 });
 
 
@@ -163,27 +206,28 @@ module.exports.updatePatient=asyncHandler(async(req,res)=> {
 
 */
 
-module.exports.deletePatients  =asyncHandler(async (req,res)=> {
+module.exports.deletePatients = asyncHandler(async (req, res) => {
 
     const patient = await Patient.findById(req.params.id);
 
-        if(!patient){
-        
-            res.status(404).json({message:'The patient with the given ID was not found.'})
-        }
-        if(req.user.IsAdmin || req.user.id === patient.user.toString())
-        {
-            await Patient.findByIdAndDelete(req.params.id);
-            await MRIScan.deleteMany({
-                patientId:patient._id
-            });
-            res.status(200).json({message : 'is delete',
-        patientId: patient._id});
-        }
-        else{
-            res.status(403).json({message:"access denied,forbidden"});
-        }
+    if (!patient) {
+
+        res.status(404).json({ message: 'The patient with the given ID was not found.' })
     }
+    if (req.user.IsAdmin || req.user.id === patient.user.toString()) {
+        await Patient.findByIdAndDelete(req.params.id);
+        await MRIScan.deleteMany({
+            patientId: patient._id
+        });
+        res.status(200).json({
+            message: 'is delete',
+            patientId: patient._id
+        });
+    }
+    else {
+        res.status(403).json({ message: "access denied,forbidden" });
+    }
+}
 );
 
 /** 
@@ -194,10 +238,10 @@ module.exports.deletePatients  =asyncHandler(async (req,res)=> {
 
 */
 
-module.exports.countPatients  =asyncHandler(async (req,res)=> {
+module.exports.countPatients = asyncHandler(async (req, res) => {
 
     const count = await Patient.countDocuments();
-            res.status(200).json(count);
+    res.status(200).json(count);
 }
 
 );
--- routes/pateint.js
+++ routes/pateint.js
@@ -1,27 +1,29 @@
-const express=require('express');
-const router= express.Router();
-const{getAllPatient,getPatientByID,countPatients,getPatients,updatePatient,addPatient,deletePatients}=require("../controller/pateintController");
-const validateObjectId=require("../middlewares/validateObjectId");
-const{verifytoken}=require("../middlewares/verifyToken")
+const express = require('express');
+const router = express.Router();
+const { getAllPatient, getPatientByID, countPatients, getPatients, updatePatient, addPatient, deletePatients ,paginationPatients} = require("../controller/pateintController");
+const validateObjectId = require("../middlewares/validateObjectId");
+const { verifytoken, verifyTokenAndOnlyuser } = require("../middlewares/verifyToken")
 
 
 
 //http methods 
 
-router.route( '/' )
-    .get(getAllPatient) // get all the data from database
-    .get(getPatients)
-    .post(verifytoken,addPatient);
-    router.route( '/count' )
+router.route('/')
+    .get(verifytoken, paginationPatients) // get all the data from database
+    //.get(verifytoken, getPatients)
+
+
+    .post(verifytoken, addPatient);
+router.route('/count')
     .get(countPatients);
-router.route( '/:id' )
-.get(validateObjectId,getPatientByID)
-.put(validateObjectId,verifytoken,updatePatient)
-.delete(validateObjectId,verifytoken,deletePatients);
+router.route('/:id')
+    .get(validateObjectId, verifytoken, getPatientByID)
+    .put(validateObjectId, verifytoken, updatePatient)
+    .delete(validateObjectId, verifytoken, deletePatients);
 
 
 
 
 
 
-module.exports=router;
+module.exports = router;
